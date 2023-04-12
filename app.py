b75b110726c9c7388dd18cad6d1d18ee79b37f92
markpakingan <116990353+markpakingan@users.noreply.github.com>
2023-04-11 22:26:02 -0700

    added login capability

---
+++ app.py
@@ -0,0 +1,138 @@
+import os
+
+from flask import Flask, render_template, request, flash, redirect, session, g, abort
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+
+from forms import UserAddForm, LoginForm
+from models import db, connect_db, Game, User, Image, Video, Review, Game_Gamelist, GameList
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+app.app_context().push()
+
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///boardgame'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+##############################################################################
+# User signup/login/logout
+
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session: 
+        g.user = User.query.get(session[CURR_USER_KEY])
+        # print("g.user is set:", g.user)
+    else: 
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+
+    form = UserAddForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+                image_url=form.image_url.data
+            )
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already taken", 'danger')
+            return render_template('users/signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('users/signup.html', form=form)
+    
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('users/login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+
+    #implement this
+
+    flash ("You have been logout!")
+    do_logout()
+    return redirect ("/")
+
+##############################################################################
+
+@app.route('/')
+def homepage():
+    
+    # """if user is verified, show their dashboard, if not, let them sign-up"""
+
+    if g.user: 
+        # print("g.user is not None:", g.user)
+        return render_template ("home.html")
+    
+    else:
+
+        return render_template ("home-anon.html")
+
---
+++ models.py
@@ -0,0 +1,135 @@
+"""SQLAlchemy models for Warbler."""
+
+from datetime import datetime
+
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+
+class Game(db.Model):
+    """Games table"""
+
+    __tablename__ = "games"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    game_sku_id = db.Column (db.Text, nullable = False)
+    name = db.Column(db.Text, nullable = False )
+    description = db.Column(db.Text, nullable = False )
+    lowest_price = db.Column(db.Float, nullable = False )
+    MSRP = db.Column(db.Float, nullable = False )
+    image_url = db.Column(db.Text, nullable = False )
+    min_play = db.Column(db.Integer, nullable = False )
+    max_play = db.Column(db.Integer, nullable = False )
+    mechanics = db.Column(db.Text, nullable = False )
+    artists= db.Column(db.Text, nullable = False )
+
+    gamelists = db.relationship("GameList", secondary = "game_gamelists", backref = "games")
+
+class Video(db.Model):
+
+    __tablename__= "videos"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    url = db.Column (db.Text, nullable = False)
+    games_id = db.Column(db.Integer, db.ForeignKey("games.id"), nullable = False )
+
+class Image(db.Model):
+
+    __tablename__= "images"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    url = db.Column (db.Text, nullable = False)
+    games_id = db.Column(db.Integer, db.ForeignKey("games.id"), nullable = False )
+
+class Review(db.Model):
+
+    __tablename__= "reviews"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False )
+    game_id = db.Column(db.Integer, db.ForeignKey("games.id"), nullable = False )
+    rating = db.Column (db.Integer, nullable = False)
+    feedback = db.Column (db.Text, nullable = False)
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column (db.Integer, primary_key = True, autoincrement = True)
+    username = db.Column (db.Text, nullable = False)
+    password = db.Column (db.Text, nullable = False)
+    image_url = db.Column (db.Text, nullable = True)
+    email = db.Column(db.Text, nullable = False)
+
+
+class GameList(db.Model):
+
+    __tablename__ = "gamelists"
+
+    id = db.Column (db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column (db.Text, nullable = False)
+    description = db.Column (db.Text, nullable = False)
+    user_id = db.Column (db.Text, db.ForeignKey("users.id"),nullable = False)
+
+
+    @classmethod
+    def signup(cls, username, email, password, image_url):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            image_url=image_url,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+
+class Game_Gamelist (db.Model):
+
+    __tablename__ = "game_gamelists"
+
+    id = db.Column (db.Integer, primary_key = True, autoincrement = True)
+    game_id = db.Column (db.Integer, db.ForeignKey("games.id"), nullable = False)
+    user_id = db.Column (db.Integer, db.ForeignKey("users.id"), nullable = False)
+
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
